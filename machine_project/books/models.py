afc3e1f1cd429839d19ed0ad7ac412566f857a03
Thomasraf <thomas_cruz@dlsu.edu.ph>
2020-04-07 20:49:34 +0800

    Trying to fix

--- machine_project/books/models.py
+++ machine_project/books/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Books(models.Model):
-    title: models.CharField()
-    author: models.TextField()
-    publisher: models.TextField()
-    yearOfPublication: models.DateField()
-    isbn: models.IntegerField()
-    deweyDecimalSystem: models.IntegerField(max_length=3)
-    status: models.BooleanField(default=False)
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    publisher = models.TextField()
+    yearOfPublication = models.DateTimeField(default = timezone.now)
+    isbn = models.IntegerField()
+    deweyDecimalSystem = models.CharField(max_length=3)
+    status = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
