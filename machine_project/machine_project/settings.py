c613f969b413b83df84a16ebe7d65c413fe79c23
Thomasraf <thomas_cruz@dlsu.edu.ph>
2020-04-08 17:27:13 +0800

    Register

--- machine_project/machine_project/settings.py
+++ machine_project/machine_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'library.apps.LibraryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
